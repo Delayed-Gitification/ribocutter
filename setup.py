f3b87684f478efbf4b15262a9dad2686e44eff73
Oscar <oscargwilkins@gmail.com>
2021-07-13 11:17:50 +0100

    0.0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 # This call to setup() does all the work
 setup(
     name="ribocutter",
-    version="0.0.7",
+    version="0.0.8",
     description="Design oligos to produce sgRNAs for abundant sequences in a fastq file",
     #long_description=README,
     long_description_content_type="text/markdown",
